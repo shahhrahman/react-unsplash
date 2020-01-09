6f76f38921efb342f987e64f14ccaa38c8b6db61
shahhrahman <shahhabibur1@gmail.com>
2020-01-08 22:25:48 -0500

    Add functional props and handle form submittal

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-class App extends React.Component {
-    render(){
-        return <div>Hello there!</div>
-    }
-}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
