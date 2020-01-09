25f9c59d5bd9782766e8de9a732f41bd54848e4c
shahhrahman <shahhabibur1@gmail.com>
2020-01-09 16:40:41 -0500

    Configure Axios to get image url list

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit = (term) => {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 61ffa6ee2caba0bdd5eeadb8a72aef958da21d0643824ed418fe6fa036f3d6c9'
+            }
+        });
     }
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         );
     }
